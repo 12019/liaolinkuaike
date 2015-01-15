a74780db1565dfbb005ca84306c9ad0664d4c8e5
ljh0925 <15524884491@163.com>
2015-01-15 11:13:29 +0800

    ver0.1.0目前版本只支持刷卡,不打印票据。2015年1月12日  作者刘及华

---
+++ o/D/main.d
@@ -0,0 +1,61 @@
+main.o: Project\main.c Project\/main.h Project/data_type.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/string.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/features.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/predefs.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/sys/cdefs.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/wordsize.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/gnu/stubs.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../lib/gcc/arm-none-linux-gnueabi/4.3.3/include/stddef.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/xlocale.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/string.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/string2.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/stdlib.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/string3.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/waitflags.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/waitstatus.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/endian.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/endian.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/sys/types.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/types.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/typesizes.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/time.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/sys/select.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/select.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/sigset.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/time.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/sys/sysmacros.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/pthreadtypes.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/alloca.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/stdlib.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/stdio.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/libio.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/_G_config.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/wchar.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../lib/gcc/arm-none-linux-gnueabi/4.3.3/include/stdarg.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/stdio_lim.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/sys_errlist.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/stdio.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/stdio2.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/macrodef.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/common_exp.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/MacroDef.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/sys/stat.h \
+  c:\program\ files\ept-tools\ept-sdk\sourcery\ g++\ lite\bin\../arm-none-linux-gnueabi/libc/usr/include/bits/stat.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/keyboard.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/lcd.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/pm.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/MacroDef.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/cfginfo.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/paramfile.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/sysserv.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/DiskPart.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/iconvert.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/CommonFunc.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/printer.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/MacroDef.H \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/IODrive.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/ffile.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/cfile.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/tms.h \
+  C:\Program\ Files\EPT-Tools\EPT-SDK\EPT-A9L\inc/Common_exp.h \
+  Project\/ydt.h Project\/data_type.h Project\/shell.h Project\/ymodem.h
